ac10c92f1b9ff383ac88fdf97acad38512a1269a
wangyu <wangyu@wycode.cn>
2019-11-28 19:28:35 +0800

    登录

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,23 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
+import { NzModalRef } from 'ng-zorro-antd';
+
+const MAX_TRY_COUNT = 5;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
   username: string;
   password: string;
+  loading = false;
+  error = undefined;
+  private tryCount = 0;
 
-  constructor() {
+  constructor(private authService: AuthService, private modal: NzModalRef) {
   }
 
-  ngOnInit(): void {
-  }
 
   submitForm(): void {
-    console.log('submitForm->', this.password, this.password);
+    if (this.tryCount >= MAX_TRY_COUNT - 1) {
+      this.error = '尝试次数过多！';
+      return;
+    }
+    this.loading = true;
+    this.error = undefined;
+    this.authService.login(this.username, this.password).subscribe(data => {
+      this.loading = false;
+      if (data.success) {
+        this.modal.close();
+      } else {
+        this.tryCount++;
+        this.error = `${data.error}，还能尝试${MAX_TRY_COUNT - this.tryCount}次`;
+      }
+    });
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
+import { JsonResult } from './type';
 
 const LOGIN_PATH = '/admin/user/login';
+const AUTH_KEY = 'AUTH_KEY';
 
 interface AuthUser {
   avatar: string;
@@ -16,17 +19,34 @@ interface AuthUser {
 })
 export class AuthService {
 
-  user: AuthUser;
+  private mUser: AuthUser;
 
   constructor(private http: HttpClient) {
+  }
+
+  public get user() {
+    return this.mUser && JSON.parse(window.localStorage.getItem(AUTH_KEY));
+  }
+
+  public set user(user: AuthUser) {
+    this.mUser = user;
+    window.localStorage.setItem(AUTH_KEY, JSON.stringify(user));
   }
 
   login(username: string, password: string) {
     const params = new HttpParams()
       .append('username', username)
       .append('password', password);
-    this.http.get(environment.baseUrl + LOGIN_PATH, { params }).subscribe((data) => {
-      console.log('login->', data);
-    });
+    return this.http.get(environment.baseUrl + LOGIN_PATH, { params })
+      .pipe(map((data: JsonResult<AuthUser>) => {
+        if (data.success) {
+          this.user = data.data;
+        }
+        return data;
+      }));
+  }
+
+  logout() {
+    this.user = undefined;
   }
 }
