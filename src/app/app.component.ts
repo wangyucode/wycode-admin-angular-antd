a799185bbeefd60b81318f31d90a0f90ed3ea4ee
wangyu <wangyu@wycode.cn>
2019-12-02 19:24:04 +0800

    登录

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService, AuthUser } from './service/auth.service';
-import { of } from 'rxjs/internal/observable/of';
+import { LoginComponent } from './component/login/login.component';
+import { NzModalService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isCollapsed = false;
-  userTrigger: null | 'hover' = null;
   userAvatar: string;
 
-  constructor(private authService: AuthService) {
-    of(authService.user).subscribe((user?: AuthUser) => {
+  constructor(private authService: AuthService, private modalService: NzModalService) {
+  }
+
+  ngOnInit(): void {
+    this.authService.userEmit.subscribe((user?: AuthUser) => {
       if (user) {
-        this.userTrigger = 'hover';
         this.userAvatar = user.avatar;
       } else {
-        this.userTrigger = null;
         this.userAvatar = null;
       }
     });
   }
 
+  accountClick() {
+    if (!this.userAvatar) {
+      this.modalService.create({
+        nzContent: LoginComponent,
+        nzFooter: null
+      });
+    }
+  }
+
   logout() {
     this.authService.logout();
   }
+
+
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { map } from 'rxjs/operators';
 import { JsonResult } from './type';
+import { Observable } from 'rxjs';
+import { NzMessageService } from 'ng-zorro-antd';
 
 const LOGIN_PATH = '/admin/user/login';
 const AUTH_KEY = 'AUTH_KEY';
@@ -20,8 +22,10 @@ export interface AuthUser {
 export class AuthService {
 
   private mUser: AuthUser;
+  userEmit = new EventEmitter<AuthUser>();
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private messageService: NzMessageService) {
+    this.user = JSON.parse(window.localStorage.getItem(AUTH_KEY));
   }
 
   public get user() {
@@ -31,6 +35,7 @@ export class AuthService {
   public set user(user: AuthUser) {
     this.mUser = user;
     window.localStorage.setItem(AUTH_KEY, JSON.stringify(user));
+    this.userEmit.emit(user);
   }
 
   login(username: string, password: string) {
@@ -41,6 +46,7 @@ export class AuthService {
       .pipe(map((data: JsonResult<AuthUser>) => {
         if (data.success) {
           this.user = data.data;
+          this.messageService.create('success', `欢迎回来： ${data.data.username}`);
         }
         return data;
       }));
